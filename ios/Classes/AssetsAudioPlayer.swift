b510bee0913bf9548f906b45454813edae2d6f1f
Florent Champigny <florent.champigny@idean.com>
2020-04-09 17:34:18 +0200

    added ios parallel

--- ios/Classes/AssetsAudioPlayer.swift
+++ ios/Classes/AssetsAudioPlayer.swift
@@ -13,18 +13,13 @@ public class SwiftAssetsAudioPlayerPlugin: NSObject, FlutterPlugin {
   }
 }
 
-class Music : NSObject, AVAudioPlayerDelegate {
+public class Player : NSObject, AVAudioPlayerDelegate {
     
-    static let METHOD_POSITION = "player.position"
-    static let METHOD_FINISHED = "player.finished"
-    static let METHOD_IS_PLAYING = "player.isPlaying"
-    static let METHOD_CURRENT = "player.current"
-    static let METHOD_VOLUME = "player.volume"
-
     let channel: FlutterMethodChannel
     let registrar: FlutterPluginRegistrar
-    init(messenger: FlutterBinaryMessenger, registrar: FlutterPluginRegistrar) {
-        self.channel = FlutterMethodChannel(name: "assets_audio_player", binaryMessenger: messenger);
+    
+    init(channel: FlutterMethodChannel, registrar: FlutterPluginRegistrar) {
+        self.channel = channel
         self.registrar = registrar
     }
     
@@ -32,147 +27,67 @@ class Music : NSObject, AVAudioPlayerDelegate {
         channel.invokeMethod("log", arguments: message)
     }
     
-    func start(){
-        channel.setMethodCallHandler({(call: FlutterMethodCall, result: FlutterResult) -> Void in
-            self.log(call.method + call.arguments.debugDescription)
-            switch(call.method){
-            case "isPlaying" :
-                result(self.playing); break;
-            case "play" : self.play();result(true); break;
-            case "pause" : self.pause();result(true); break;
-            case "stop" : self.stop();result(true); break;
-            case "seek" : if(call.arguments is Int) {
-                let pos = call.arguments as! Int;
-                self.seek(to: pos);
-                result(true);
-            } else {
-                result(FlutterError(code: "WRONG_FORMAT",
-                                    message: "The specified argument for seek must be an Int.",
-                                    details: nil))
+    func open(assetPath: String, autoStart: Bool, result: FlutterResult){
+            let assetKey = registrar.lookupKey(forAsset: assetPath)
+            guard let path = Bundle.main.path(forResource: assetKey, ofType: nil) else {
+                 log("resource not found \(assetKey)")
+                 result("");
+                 return
             }
-            break;
-            case "volume" : if(call.arguments is Double) {
-                let volume = call.arguments as! Double;
-                self.setVolume(volume: volume);
-                result(true);
-            } else {
-                result(FlutterError(code: "WRONG_FORMAT",
-                                    message: "The specified argument for volume must be an Double.",
-                                    details: nil))
-            }
-            break;
-            case "open" :
-                let args = call.arguments as! NSDictionary
-                let assetPath = args["path"] as! String
-                let autoStart = args["autoStart"] as! Bool
-                self.open(assetPath: assetPath, autoStart: autoStart, result: result);
-               
-                break;
+            
+            let url = URL(fileURLWithPath: path)
+    //        log("url: "+url.absoluteString)
+            do {
                 
-            default:
-                result(FlutterMethodNotImplemented);
-                break;
+                /* set session category and mode with options */
+                if #available(iOS 10.0, *) {
+                    try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [])
+                } else {
+                    try AVAudioSession.sharedInstance().setCategory(.playback, options: .mixWithOthers)
+                }
                 
-            }
-        })
-    }
-    
-    var _currentTime : TimeInterval = 0
-    private var currentTime : TimeInterval {
-        get {
-            return _currentTime
-        }
-        set(newValue) {
-            _currentTime = newValue
-            self.channel.invokeMethod(Music.METHOD_POSITION, arguments: self._currentTime)
-        }
-    };
-    
-    var _playing : Bool = false
-    private var playing : Bool {
-        get {
-            return _playing
-        }
-        set(newValue) {
-            _playing = newValue
-            self.channel.invokeMethod(Music.METHOD_IS_PLAYING, arguments: self._playing)
-        }
-    };
-    
-    var currentTimeTimer: Timer?
-    var player: AVAudioPlayer?
-    
-    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool){
-        self.channel.invokeMethod(Music.METHOD_FINISHED, arguments: true)
-    }
-    
-   func open(assetPath: String, autoStart: Bool, result: FlutterResult){
-        let assetKey = registrar.lookupKey(forAsset: assetPath)
-        guard let path = Bundle.main.path(forResource: assetKey, ofType: nil) else {
-             log("resource not found \(assetKey)")
-             result("");
-             return
-        }
-        
-        let url = URL(fileURLWithPath: path)
-//        log("url: "+url.absoluteString)
-        do {
-            
-            /* set session category and mode with options */
-            if #available(iOS 10.0, *) {
-                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [])
-            } else {
-                try AVAudioSession.sharedInstance().setCategory(.playback, options: .mixWithOthers)
-            }
-            
-            try AVAudioSession.sharedInstance().setActive(true)
-            
-            /* The following line is required for the player to work on iOS 11. Change the file type accordingly */
-            if #available(iOS 11.0, *) {
-                self.player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-            } else {
-                /* iOS 10 and earlier require the following line: */
-                self.player = try AVAudioPlayer(contentsOf: url)
-            }
-            
-            if(self.player == nil){
-                //log("player is null");
-                return
-            }
-            
-            self.player?.prepareToPlay()
-            
-            self.currentTime = 0
-            self.playing = false
-            
-            self.player?.delegate = self
+                try AVAudioSession.sharedInstance().setActive(true)
+                
+                /* The following line is required for the player to work on iOS 11. Change the file type accordingly */
+                if #available(iOS 11.0, *) {
+                    self.player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+                } else {
+                    /* iOS 10 and earlier require the following line: */
+                    self.player = try AVAudioPlayer(contentsOf: url)
+                }
+                
+                if(self.player == nil){
+                    //log("player is null");
+                    return
+                }
+                
+                self.player?.prepareToPlay()
+                
+                self.currentTime = 0
+                self.playing = false
+                
+                self.player?.delegate = self
 
-            if(autoStart){
-                play()
+                if(autoStart){
+                    play()
+                }
+                
+                result(true);
+                //log("play_ok");
+                
+                let asset = AVURLAsset(url: url, options: nil)
+        
+                let audioDurationSeconds = CMTimeGetSeconds(asset.duration)
+                
+                self.channel.invokeMethod(Music.METHOD_CURRENT, arguments: ["totalDuration": audioDurationSeconds])
+                
+            } catch let error {
+                result(error);
+                log(error.localizedDescription)
+                print(error.localizedDescription)
             }
-            
-            result(true);
-            //log("play_ok");
-            
-            let asset = AVURLAsset(url: url, options: nil)
-    
-            let audioDurationSeconds = CMTimeGetSeconds(asset.duration)
-            
-            self.channel.invokeMethod(Music.METHOD_CURRENT, arguments: ["totalDuration": audioDurationSeconds])
-            
-        } catch let error {
-            result(error);
-            log(error.localizedDescription)
-            print(error.localizedDescription)
         }
-    }
-    
-    func pause(){
-        self.player?.pause()
-        self.playing = false
-        self.currentTimeTimer?.invalidate()
-    }
-    
+
     func seek(to: Int){
         self.player?.currentTime = Double(to)
     }
@@ -195,11 +110,146 @@ class Music : NSObject, AVAudioPlayerDelegate {
         self.playing = true
     }
     
+    var _currentTime : TimeInterval = 0
+    private var currentTime : TimeInterval {
+        get {
+            return _currentTime
+        }
+        set(newValue) {
+            _currentTime = newValue
+            self.channel.invokeMethod(Music.METHOD_POSITION, arguments: self._currentTime)
+        }
+    };
+    
+    var _playing : Bool = false
+    var playing : Bool {
+        get {
+            return _playing
+        }
+        set(newValue) {
+            _playing = newValue
+            self.channel.invokeMethod(Music.METHOD_IS_PLAYING, arguments: self._playing)
+        }
+    };
+    
+    var currentTimeTimer: Timer?
+    var player: AVAudioPlayer?
+    
+    public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool){
+        self.channel.invokeMethod(Music.METHOD_FINISHED, arguments: true)
+    }
+
+    
+    func pause(){
+        self.player?.pause()
+        self.playing = false
+        self.currentTimeTimer?.invalidate()
+    }
+    
     @objc func updateTimer(){
         //log("updateTimer");
         if let p = self.player {
             self.currentTime = p.currentTime
         }
+    }
+}
+
+class Music : NSObject {
+    
+    static let METHOD_POSITION = "player.position"
+    static let METHOD_FINISHED = "player.finished"
+    static let METHOD_IS_PLAYING = "player.isPlaying"
+    static let METHOD_CURRENT = "player.current"
+    static let METHOD_VOLUME = "player.volume"
+    
+    var players = Dictionary<String, Player>()
+    
+    func getOrCreatePlayer(id: String) -> Player {
+        if let player = players[id] {
+            return player
+        } else {
+            let newPlayer = Player(
+                channel: FlutterMethodChannel(name: "assets_audio_player/"+id, binaryMessenger: registrar.messenger()),
+                registrar: self.registrar
+            )
+            players[id] = newPlayer
+            return newPlayer
+        }
+    }
+
+    let channel: FlutterMethodChannel
+    let registrar: FlutterPluginRegistrar
+    
+    init(messenger: FlutterBinaryMessenger, registrar: FlutterPluginRegistrar) {
+        self.channel = FlutterMethodChannel(name: "assets_audio_player", binaryMessenger: messenger);
+        self.registrar = registrar
+    }
+    
+    func start(){
+        channel.setMethodCallHandler({(call: FlutterMethodCall, result: FlutterResult) -> Void in
+            //self.log(call.method + call.arguments.debugDescription)
+            switch(call.method){
+            case "isPlaying" :
+                let args = call.arguments as! NSDictionary
+                let id = args["id"] as! String
+                result(self.getOrCreatePlayer(id: id).playing);
+                break;
+            case "play" :
+                let args = call.arguments as! NSDictionary
+                let id = args["id"] as! String
+                self.getOrCreatePlayer(id: id)
+                    .play();
+            result(true);
+            break;
+                
+            case "pause" :
+                let args = call.arguments as! NSDictionary
+                let id = args["id"] as! String
+                self.getOrCreatePlayer(id: id)
+                    .pause();
+                result(true);
+            break;
+                
+            case "stop" :
+                let args = call.arguments as! NSDictionary
+                let id = args["id"] as! String
+                self.getOrCreatePlayer(id: id)
+                    .stop();
+                result(true);
+                break;
+            case "seek" :
+                let args = call.arguments as! NSDictionary
+                let id = args["id"] as! String
+                let pos = args["to"] as! Int;
+                self.getOrCreatePlayer(id: id)
+                    .seek(to: pos);
+                result(true);
+            break;
+                
+            case "volume" :
+                let args = call.arguments as! NSDictionary
+                let id = args["id"] as! String
+                let volume = args["volume"] as! Double;
+                self.getOrCreatePlayer(id: id)
+                    .setVolume(volume: volume);
+                result(true);
+                break;
+                
+            case "open" :
+                let args = call.arguments as! NSDictionary
+                let id = args["id"] as! String
+                let assetPath = args["path"] as! String
+                let autoStart = args["autoStart"] as! Bool
+                self.getOrCreatePlayer(id: id)
+                    .open(assetPath: assetPath, autoStart: autoStart, result: result);
+            break;
+                
+            default:
+                result(FlutterMethodNotImplemented);
+            break;
+                
+            }
+        })
     }
     
 }
