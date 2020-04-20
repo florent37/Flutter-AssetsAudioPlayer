9f995f251b80852c823b07853633b8d2e6a13177
Florent Champigny <florent.champigny@idean.com>
2020-04-20 15:29:06 +0200

    updated README

---
+++ example/lib/generated_plugin_registrant.dart
@@ -0,0 +1,18 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:assets_audio_player_web/web/assets_audio_player_web.dart';
+import 'package:import_js_library/import_js_library.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  AssetsAudioPlayerWebPlugin.registerWith(registry.registrarFor(AssetsAudioPlayerWebPlugin));
+  ImportJsLibrary.registerWith(registry.registrarFor(ImportJsLibrary));
+  registry.registerMessageHandler();
+}
