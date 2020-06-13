f725da3defcfef28df9b112da8ebec076518d270
Sumit <me.summet@gmail.com>
2020-06-13 10:39:54 +0545

    notification custom icon made nullable

--- android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationSettings.kt
+++ android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationSettings.kt
@@ -10,11 +10,11 @@ class NotificationSettings(
 
         //android only
         val stopEnabled: Boolean,
-        val previousIcon: String,
-        val stopIcon: String,
-        val playIcon: String,
-        val nextIcon: String,
-        val pauseIcon: String
+        val previousIcon: String?,
+        val stopIcon: String?,
+        val playIcon: String?,
+        val nextIcon: String?,
+        val pauseIcon: String?
 ) : Serializable {
     fun numberEnabled() : Int {
         var number = 0
@@ -33,10 +33,10 @@ fun fetchNotificationSettings(from: Map<*, *>) : NotificationSettings {
             playPauseEnabled = from["notif.settings.playPauseEnabled"] as? Boolean ?: true,
             prevEnabled = from["notif.settings.prevEnabled"] as? Boolean ?: true,
             seekBarEnabled = from["notif.settings.seekBarEnabled"] as? Boolean ?: true,
-            previousIcon = from["notif.settings.previousIcon"] as String,
-            nextIcon = from["notif.settings.nextIcon"] as String,
-            pauseIcon = from["notif.settings.pauseIcon"] as String,
-            playIcon = from["notif.settings.playIcon"] as String,
-            stopIcon = from["notif.settings.stopIcon"] as String
+            previousIcon = from["notif.settings.previousIcon"] as? String,
+            nextIcon = from["notif.settings.nextIcon"] as? String,
+            pauseIcon = from["notif.settings.pauseIcon"] as? String,
+            playIcon = from["notif.settings.playIcon"] as? String,
+            stopIcon = from["notif.settings.stopIcon"] as? String
     )
 }
