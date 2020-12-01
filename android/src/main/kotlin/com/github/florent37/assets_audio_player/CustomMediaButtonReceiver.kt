e4adbf7286d4be4f6a7ff5ec008943a23d95d501
George Leidig <georgeleidig@imac.home>
2020-12-01 08:14:50 +0000

    fix

---
+++ android/src/main/kotlin/com/github/florent37/assets_audio_player/CustomMediaButtonReceiver.kt
@@ -0,0 +1,14 @@
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.media.session.MediaButtonReceiver
+
+class CustomMediaButtonReceiver : MediaButtonReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        try {
+            super.onReceive(context, intent)
+        } catch (e: Exception) {
+            Log.e(javaClass.name, e.message)
+        }
+    }
+}
