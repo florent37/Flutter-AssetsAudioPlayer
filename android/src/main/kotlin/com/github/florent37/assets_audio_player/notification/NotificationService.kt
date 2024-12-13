f882b5ab1f47aed7649681a602a279edea585ecf
Rupak Poddar <34826905+Rupakpoddar@users.noreply.github.com>
2024-12-13 15:32:12 -0500

    Update NotificationService.kt

--- android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationService.kt
+++ android/src/main/kotlin/com/github/florent37/assets_audio_player/notification/NotificationService.kt
@@ -165,27 +165,27 @@ class NotificationService : Service() {
     }
 
     private fun getSmallIcon(context: Context): Int {
-        return getCustomIconOrDefault(context, manifestIcon, null, R.drawable.exo_icon_circular_play)
+        return android.R.drawable.ic_media_play
     }
-
+    
     private fun getPlayIcon(context: Context, resourceName: String?): Int {
-        return getCustomIconOrDefault(context, manifestIconPlay, resourceName, R.drawable.exo_icon_play)
+        return android.R.drawable.ic_media_play
     }
-
+    
     private fun getPauseIcon(context: Context, resourceName: String?): Int {
-        return getCustomIconOrDefault(context, manifestIconPause, resourceName, R.drawable.exo_icon_pause)
+        return android.R.drawable.ic_media_pause
     }
-
+    
     private fun getNextIcon(context: Context, resourceName: String?): Int {
-        return getCustomIconOrDefault(context, manifestIconNext, resourceName, R.drawable.exo_icon_next)
+        return android.R.drawable.ic_media_next
     }
-
+    
     private fun getPrevIcon(context: Context, resourceName: String?): Int {
-        return getCustomIconOrDefault(context, manifestIconPrev, resourceName, R.drawable.exo_icon_previous)
+        return android.R.drawable.ic_media_previous
     }
-
+    
     private fun getStopIcon(context: Context, resourceName: String?): Int {
-        return getCustomIconOrDefault(context, manifestIconStop, resourceName, R.drawable.exo_icon_stop)
+        return android.R.drawable.ic_media_pause
     }
 
     private fun getCustomIconOrDefault(context: Context, manifestName: String, resourceName: String?, defaultIcon: Int): Int {
